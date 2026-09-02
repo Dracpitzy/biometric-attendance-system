56c0a9d42a018bcdbde92ae2cf98422791493a28
peter <peterchukwuka926@gmail.com>
2026-09-02 01:32:35 +0100

    changes

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
+from staffs.models import staff
 
-# Create your models here.
+class AttendanceLog(models.Model):
+    class LogType(models.TextChoices):
+        CHECK_IN = "check_in", "Check In"
+        CHECK_OUT = "check_out", "Check Out"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name="attendance_logs")
+    log_type = models.CharField(max_length=10, choices=LogType.choices)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f"{self.staff.staff_id} - {self.log_type} @ {self.timestamp}"
+
+
+
+
